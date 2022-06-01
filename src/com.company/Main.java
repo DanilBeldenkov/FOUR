8103983fbfb9452c14366c7bb6ec3c040607326f
Danil <pingvin5332@gmail.com>
2022-06-01 14:51:09 +0300

    Commit

---
+++ src/com.company/Main.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        final double v = 345.0;
+        final double t = 6.8;
+        double s;
+        s = t * v;
+        System.out.println("Расстояние до места удара молнии: " + s + " метров");
+    }
+}
